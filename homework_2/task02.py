f24ba47d716c8ffa9cc9e9a8e16570540e0293a5
volevanya <7323649aAqQ1!1>
2021-06-23 13:05:23 +0300

    ID: HW2
    done 2st task

---
+++ homework_2/task02.py
@@ -0,0 +1,10 @@
+# У вас есть 3 группы людей bigno_blacklist, poker_blacklist, majong_blacklist.
+# Имена людей в формате "John Dow" первое и второе имя.
+# Найти тех кто состоит во всех черных списках.
+
+bigno_blacklist = {"Mac DeMarco", "Franchesko Papa", "Vasya Pupkin"}
+poker_blacklist = {"Clarc Pupenberg", "Petya pyatochkin", "Mac DeMarco", "Franchesko Papa"}
+majong_blacklist = {"PVasya Pupkin", "Mac DeMarco", "Franchesko Papa"}
+
+totally_banned = bigno_blacklist.intersection(poker_blacklist).intersection(majong_blacklist)
+print(totally_banned)
