9b24ecd16eefc5e7f37cab308f8a62fd8d73cf40
volevanya <7323649aAqQ1!1>
2021-06-23 13:22:16 +0300

    ID: HW2
    done 4th task

---
+++ homework_2/task04.py
@@ -0,0 +1,19 @@
+# У вас есть группа гостей для вип ложи.
+# Места в вип ложе все заняты этими гостями.
+# Есть группа гостей в общем зале и места в нем все еще есть.
+# Отобразите 2 группы гостей в коде.
+
+vip_lounge = {
+    "table01": "Masha Prostokvasha",
+    "table02": "Sanya Parizanya",
+    "table03": "Petya Chiketya"
+}
+
+normal_lounge = {
+    "table01": "Vasya Pupkin",
+    "table02": "Dasha Kakasha",
+    "table03": None
+}
+
+print(vip_lounge)
+print(normal_lounge)
